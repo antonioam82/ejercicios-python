1801f1971af229e8450a0efce1e01338f550128d
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-07-14 14:30:46 +0200

    Create copiador.py

---
+++ copiador.py
@@ -0,0 +1,36 @@
+from shutil import copyfile
+from sys import exit
+
+def ns(c):
+    while c!=("s") and c!=("n"):
+        print(chr(7));c=input("Escribe solo \'n\' o \'s\' según su opción: ")
+    return(c)
+
+
+origen = input("Introduce ruta del archivo a copiar: ")
+destino = input("Introduce ruta de la carpeta de destino: ")
+
+try:
+    copyfile(origen, destino)
+except IOError as e:
+    print("No se pudo copiar el archivo. %s" % e)
+    exit(1)
+except:
+    print("Error:", sys.exc_info())
+    exit(1)
+
+print("\nCopia realizada con exito\n")
+
+while True:
+    comprobar=ns(input("Desea visualizar el archivo copiado?: "))
+    if comprobar ==("n"):
+        break
+    elif comprobar == 's':
+        archivo = open(origen, "r")
+        print("\nContenido:\n")
+        print(archivo.read())
+        archivo.close()
+        print()
+        break
+    else:
+        continue
