48e2d7b469c8a4c9586fcc7dc2cdb8ad93b1bb9d
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-02-21 23:28:52 +0100

    Create hexa_to_binar.py

---
+++ hexa_to_binar.py
@@ -0,0 +1,34 @@
+from VALID import ns
+import subprocess
+
+lista_hex=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+
+while True:
+    print("****************TRADUCTOR HEXADECIMAL-BINARIO****************")
+    print("")
+    hexa=input("Introduce hexadecimal: ")
+
+    binario=[]
+
+    for i in hexa:
+        if i in lista_hex:
+            n=lista_hex.index(i)
+            num_binar=(bin(n)).lstrip("0b")
+            longi=(abs(len(num_binar)-4))
+            n='0'*longi
+            n_cadena=n+num_binar
+            binario.append(n_cadena)
+        else:
+            print("El carater",i,"no es hexadecimal, por lo que ha sido ignorado.")
+
+    binario_final=str(("").join(binario))
+    
+    print("")
+    print("TRADUCCIÓN BINARIA:",binario_final)
+    print("")
+
+    conti=ns(input("¿Desea continuar?: "))
+    if conti=="n":
+        break
+    subprocess.call(["cmd.exe","/C","cls"])
+    
