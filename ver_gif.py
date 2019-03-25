b4e38b05657fdb140bb42f4d739287c234ecd6b9
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-03-25 15:44:56 +0100

    Create ver_gif.py

---
+++ ver_gif.py
@@ -0,0 +1,20 @@
+from pyglet.window import key
+import pyglet
+import os
+    
+animation = pyglet.image.load_animation('walk.gif')
+bin = pyglet.image.atlas.TextureBin()
+animation.add_to_texture_bin(bin)
+sprite = pyglet.sprite.Sprite(animation)
+
+w = sprite.width
+h = sprite.height
+
+window = pyglet.window.Window(width=w, height=h)
+
+@window.event
+def on_draw():
+    sprite.draw()
+
+
+pyglet.app.run()
