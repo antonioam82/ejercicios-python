3a5ff3ae8d40be422df0a1b26d4725394043f2b8
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-04-22 14:17:42 +0200

    Update live_graph.py

--- live_graph.py
+++ live_graph.py
@@ -1,9 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
-import os
-
-os.chdir(r'C:\Users\Antonio\Documents\docs')
 
 style.use('fivethirtyeight')
 
@@ -12,13 +9,6 @@ ax1 = fig.add_subplot(111)
 
 def animate(i):
     graph_data = open('ejemplo.txt','r').read()
-    #count=0
-    #for linea in graph_data:
-        #count+=1
-    #if count>1:
-        #print(count)
-    #else:
-        #print("n")
     lines = graph_data.split('\n')
     xs = []
     ys = []
@@ -27,7 +17,7 @@ def animate(i):
             x, y = line.split(',')
             xs.append(x)
             ys.append(y)
-    #print("22")
+    
        
     ax1.clear()
     ax1.plot(xs, ys)
