50e07144d74df2200401ae62bde4d94603d70f45
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-05-08 20:11:08 +0200

    Update painty.py

--- painty.py
+++ painty.py
@@ -11,33 +11,56 @@ def back():
 
 def left():
     t.left(90)
-    #t.forward(100)
+
 
 def right():
     t.right(90)
-    #t.forward(100)
+
 
 def colore(c):
     global col
     t.pencolor(c)
     col = c
 
+def size(s):
+    t.pensize(s)
+
+def color_fondo(cl):
+    t.screen.bgcolor(cl)
+
 root = tk.Tk()
-canvas = tk.Canvas(master = root, width = 600, height = 600)
+canvas = tk.Canvas(master = root, width = 700, height = 700)
 canvas.pack()
 
 t = turtle.RawTurtle(canvas)
 t.pencolor("black") # Red #ff0000
 col = "black"
-t.penup()   # Regarding one of the comments
-t.pendown() # Regarding one of the comments
 
+#BOTONES DE DIRECCION
 tk.Button(master = root, text = "Forward", command = forward).pack(side = tk.LEFT)
 tk.Button(master = root, text = "Back", command = back).pack(side = tk.LEFT)
 tk.Button(master = root, text = "Left", command = left).pack(side = tk.LEFT)
 tk.Button(master = root, text = "Right", command = right).pack(side = tk.LEFT)
-tk.Button(master = root, bg = "blue", command = lambda:colore("blue") ).pack(side = tk.RIGHT)
-tk.Button(master = root, bg = "red", command = lambda:colore("red") ).pack(side = tk.RIGHT)
-tk.Button(master = root, bg = "green", command = lambda:colore("green") ).pack(side = tk.RIGHT)
-tk.Button(master = root, bg = "black", command = lambda:colore("black") ).pack(side = tk.RIGHT)
+#BOTONES PARA COLOR DE LINEA
+tk.Button(master = root, bg = "blue", width=2, command = lambda:colore("blue")).pack(side = tk.RIGHT)
+tk.Button(master = root, bg = "red", width=2, command = lambda:colore("red") ).pack(side = tk.RIGHT)
+tk.Button(master = root, bg = "green", width=2, command = lambda:colore("green") ).pack(side = tk.RIGHT)
+tk.Button(master = root, bg = "yellow", width=2, command = lambda:colore("yellow") ).pack(side = tk.RIGHT)
+tk.Button(master = root, bg = "black", width=2, command = lambda:colore("black") ).pack(side = tk.RIGHT)
+tk.Label(master = root, text = "COLOR").pack(side = tk.RIGHT)
+#BOTONES PARA GROSOR DE LINEA
+tk.Button(master = root, text = 1, width=2, command = lambda:size(1)).pack(side = tk.RIGHT)
+tk.Button(master = root, text = 3, width=2, command = lambda:size(3)).pack(side = tk.RIGHT)
+tk.Button(master = root, text = 5, width=2, command = lambda:size(5)).pack(side = tk.RIGHT)
+tk.Button(master = root, text = 7, width=2, command = lambda:size(7)).pack(side = tk.RIGHT)
+tk.Button(master = root, text = 10, width=2, command = lambda:size(10)).pack(side = tk.RIGHT)
+tk.Label(master = root, text = "GROSOR").pack(side = tk.RIGHT)
+#BOTONES PARA COLOR DE FONDO
+tk.Button(master = root, bg = "blue", width=2, command = lambda:color_fondo("blue")).pack(side = tk.RIGHT)
+tk.Button(master = root, bg = "red", width=2, command = lambda:color_fondo("red") ).pack(side = tk.RIGHT)
+tk.Button(master = root, bg = "green", width=2, command = lambda:color_fondo("green") ).pack(side = tk.RIGHT)
+tk.Button(master = root, bg = "yellow", width=2, command = lambda:color_fondo("yellow") ).pack(side = tk.RIGHT)
+tk.Button(master = root, bg = "black", width=2, command = lambda:color_fondo("black") ).pack(side = tk.RIGHT)
+tk.Label(master = root, text = "COLOR FONDO").pack(side = tk.RIGHT)
+
 root.mainloop()
