92e16b7f5ffac63c304c8fb24d6079a5a486e3f1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-05-24 09:00:29 +0200

    Create square_anim.py

---
+++ square_anim.py
@@ -0,0 +1,48 @@
+import pygame, sys
+import os
+from pygame.locals import *
+
+os.chdir(r'C:\Users\Antonio\Documents\docs')
+
+pygame.init()
+
+FPS = 30
+fpsClock = pygame.time.Clock()
+
+
+DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
+pygame.display.set_caption('Animation')
+
+WHITE = (255, 255, 255)
+catImg = pygame.image.load('anger_square.png')
+catx = 10
+caty = 10
+direction = 'right'
+
+while True: # the main game loop
+    DISPLAYSURF.fill(WHITE)
+    if direction == 'right':
+        catx += 5
+        if catx == 280:
+            direction = 'down'
+    elif direction == 'down':
+        caty += 5
+        if caty == 220:
+            direction = 'left'
+    elif direction == 'left':
+        catx -= 5
+        if catx == 10:
+            direction = 'up'
+    elif direction == 'up':
+        caty -= 5
+        if caty == 10:
+            direction = 'right'
+
+    DISPLAYSURF.blit(catImg, (catx, caty))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
