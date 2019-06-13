1a06e99a7bd27cf16c2bc66738cc0271406547e8
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-06-13 17:12:44 +0200

    Create cesar_avanzado.py

---
+++ cesar_avanzado.py
@@ -0,0 +1,47 @@
+
+LETTERS = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+def main():
+    myMessage=input("Mensaje: ")
+    myKey='ASIMOV'
+    myMode=input("Mode: ")
+
+    if myMode=='encrypt':
+        translated=encryptMessage(myKey,myMessage)
+    elif myMode=='decrypt':
+        translated=decryptMessage(myKey,myMessage)
+    print(translated)
+
+def encryptMessage(key,message):
+    return translateMessage(key,message,'encrypt')
+
+def decryptMessage(key,message):
+    return translateMessage(key,message,'decrypt')
+
+def translateMessage(key,message,mode):
+    translated=[]
+    keyIndex=0
+    key=key.upper()
+
+    for symbol in message:
+        num=LETTERS.find(symbol.upper())
+        if num!=-1:
+            if mode=='encrypt':
+                num+=LETTERS.find(key[keyIndex])
+            elif mode=='decrypt':
+                num-=LETTERS.find(key[keyIndex])
+            num%=len(LETTERS)
+            if symbol.isupper():
+                translated.append(LETTERS[num])
+            elif symbol.islower():
+                translated.append(LETTERS[num].lower())
+            keyIndex+=1
+            if keyIndex==len(key):
+                keyIndex=0
+
+        else:
+            translated.append(symbol)
+    return ('').join(translated)
+
+if __name__ == '__main__':
+    main()
