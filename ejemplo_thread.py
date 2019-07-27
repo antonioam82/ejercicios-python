a151fa346ac07bbb1fde34cfc94a2f89aff26d8c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-07-27 20:55:38 +0200

    Create ejemplo_thread.py

---
+++ ejemplo_thread.py
@@ -0,0 +1,21 @@
+import threading
+import time
+
+
+#FUNCIÓN A EJECUTAR
+def cuenta(n, name):
+    count=n
+    while count<10:
+        print("")
+        print(count)
+        count+=1
+
+#CREAMOS PROCESOS A EJECUTAR EN PARALELO.        
+t = threading.Thread(target = cuenta, args =(1, 'thread1') )
+t2 = threading.Thread(target = cuenta, args =(2, 'thread2') )
+t3 = threading.Thread(target = cuenta, args =(3, 'thread3') )
+
+#INICIAMOS PROCESOS.
+t.start()
+t2.start()
+t3.start()
