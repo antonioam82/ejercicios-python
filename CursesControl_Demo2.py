eb4e755b64765db5519e210cae6989d9de19b6f1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-10-18 19:50:03 +0200

    Update CursesControl_Demo2.py

--- CursesControl_Demo2.py
+++ CursesControl_Demo2.py
@@ -1,5 +1,6 @@
 import curses
 import time
+import random
 from curses import textpad
 
 stdscr = curses.initscr()
@@ -9,23 +10,39 @@ curses.cbreak()
 stdscr.keypad(True)
 
 def main(stdscr):
+    texto="CENTRE"
     sh, sw = stdscr.getmaxyx()
     posicion1=sh//2
     posicion2=sw//2
     #def main(stdscr):
-    stdscr.addstr(posicion1,posicion2,"@")
+    stdscr.addstr(posicion1,(posicion2)-((len(texto))//2),texto)
+    wp=[]
+    for i in range(12):
+        n=random.randint(1,28)
+        m=random.randint(1,116)
+        stdscr.addstr(n,m,"WALL")
+        wp.append((n,m))
     
     while True:
         key=stdscr.getch()
         if key==curses.KEY_RIGHT:
             posicion2=posicion2+1
+            texto="RIGHT"
         elif key==curses.KEY_DOWN:
             posicion1=posicion1+1
+            texto="DOWN"
         elif key==curses.KEY_LEFT:
             posicion2=posicion2-1
+            texto="LEFT"
         elif key==curses.KEY_UP:
             posicion1=posicion1-1
-        stdscr.addstr(posicion1,posicion2,"@")
+            texto="UP"
+        current_p=(posicion1,posicion2)
+        if current_p in wp:
+            stdscr.clear()
+            texto="CRASH!"
+        #stdscr.clear()
+        stdscr.addstr(posicion1,(posicion2)-((len(texto))//2),texto)
     
 main(stdscr)
 
@@ -37,3 +54,6 @@ curses.curs_set(1)
 curses.echo()
 curses.nocbrek()
 stdscr.keypad(False)
+
+
+curses.endwin()
