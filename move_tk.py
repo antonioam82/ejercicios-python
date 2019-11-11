83a4b2d12f47a0b2e717b8586c864668d6df452d
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-11-11 23:43:20 +0100

    Update move_tk.py

--- move_tk.py
+++ move_tk.py
@@ -5,7 +5,7 @@ class GFG:
     def __init__(self, master = None): 
         self.master = master 
 
-        #CREAR MOVIMIENTO. 
+        #CORDENADAS INICIALES. 
         self.x = 1
         self.y = 0
   
@@ -13,7 +13,10 @@ class GFG:
         self.canvas = Canvas(master) 
         #CREAR RECTANGULO
         self.rectangle = self.canvas.create_rectangle( 
-                         5, 5, 25, 25, fill = "black") 
+                         5, 5, 25, 25, fill = "black")
+        #COLOR FONDO.
+        self.canvas['bg'] = 'green'
+        
         self.canvas.pack() 
   
   
@@ -21,32 +24,31 @@ class GFG:
         self.movimiento() 
       
     def movimiento(self):
-
         #MOVER FIGURA
         self.canvas.move(self.rectangle, self.x, self.y) 
   
         self.canvas.after(100, self.movimiento) 
       
     #MOVIMIENTO A LA IZQUIERDA
-    def izquierda(self, event): 
+    def izquierda(self, event):
         print(event.keysym) 
         self.x = -5
         self.y = 0
       
     #MOVIMIENTO A LA DERECHA 
-    def derecha(self, event): 
+    def derecha(self, event):
         print(event.keysym) 
         self.x = 5
         self.y = 0
       
     #MOVIMIENTO HACIA ARRIBA 
-    def arriba(self, event): 
+    def arriba(self, event):
         print(event.keysym) 
         self.x = 0
         self.y = -5
       
     #MOVIMIENTO HACIA ABAJO 
-    def abajo(self, event): 
+    def abajo(self, event):
         print(event.keysym) 
         self.x = 0
         self.y = 5
@@ -54,8 +56,9 @@ class GFG:
 if __name__ == "__main__": 
   
     #MOSTRAR VENTANA
-    master = Tk() 
+    master = Tk()
     master.title("Move_tk")
+    master.configure(bg="green")
     gfg = GFG(master) 
 
     #CONTROLAR MOVIMIENTO POR TECLADO
