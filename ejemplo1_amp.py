db1660707ee8a570d7216995dfb2bb4a31f589c5
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-11-26 10:42:41 +0100

    Create ejemplo1_amp.py

---
+++ ejemplo1_amp.py
@@ -0,0 +1,33 @@
+#IMPORTAMOS LIBRERIAS.
+import numpy as np
+import matplotlib.pyplot as plt
+import animatplot as amp
+
+#INTRODUCIMOS DATOS.
+x = np.linspace(0, 1, 50)
+t = np.linspace(0, 1, 20)
+
+X, T = np.meshgrid(x, t)
+Y = np.sin(2*np.pi*(X+T))
+
+#CREAMOS OBJETO "timeline".
+timeline = amp.Timeline(t, units='s', fps=20)
+
+#GENERAMOS ANIMACIÓN.
+block = amp.blocks.Line(X, Y, marker=".", linestyle="-", color="r")
+anim = amp.Animation([block],timeline)
+
+#DEFINICIÓN DE ETIQUETAS PARA TITULO Y EJES.
+plt.title("Sine Wave")
+plt.xlabel("x")
+plt.ylabel("y")
+
+#GUARDAMOS ANIMACIÓN.
+anim.save_gif('graph_anim.gif') # save animation for docs
+
+#INTRODUCIMOS LÍNEA DE TIEMPO
+#Y BOTÓN PAUSE/PLAY
+anim.controls()
+
+#REPRESENTAMOS GRÁFICA.
+plt.show()
