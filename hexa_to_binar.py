bb33c0ba80d23d9c3e94bafb96a74efa8c505beb
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-09 10:53:59 +0100

    Update hexa_to_binar.py

--- hexa_to_binar.py
+++ hexa_to_binar.py
@@ -1,34 +1,32 @@
-from VALID import ns
-import subprocess
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-lista_hex=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+def ns(c):
+    while c!=("s") and c!=("n"):
+        print(chr(7));c=input("Escribe solo \'n\' o \'s\' según su opción: ")
+    return(c)
+
+#CORRESPONDENCIA HEXADECIMAL-BINARIO
+dic_hex={'0':'0000','1':'0001','2':'0010','3':'0011','4':'0100','5':'0101',
+        '6':'0110','7':'0111','8':'1000','9':'1001','A':'1010','B':'1011',
+        'C':'1100','D':'1101','E':'1110','F':'1111'}
 
 while True:
-    print("****************TRADUCTOR HEXADECIMAL-BINARIO****************")
-    print("")
+    print("****************CONVERSOR HEXADECIMAL-BINARIO****************\n")
     hexa=input("Introduce hexadecimal: ")
 
-    binario=[]
+    binario=""
 
     for i in hexa:
-        if i in lista_hex:
-            n=lista_hex.index(i)
-            num_binar=(bin(n)).lstrip("0b")
-            longi=(abs(len(num_binar)-4))
-            n='0'*longi
-            n_cadena=n+num_binar
-            binario.append(n_cadena)
+        if i in dic_hex:
+            binario=binario+dic_hex[i]
         else:
             print("El carater",i,"no es hexadecimal, por lo que ha sido ignorado.")
 
-    binario_final=str(("").join(binario))
     
-    print("")
-    print("TRADUCCIÓN BINARIA:",binario_final)
-    print("")
+    print("\nTRADUCCIÓN BINARIA:",binario)
 
-    conti=ns(input("¿Desea continuar?: "))
+    conti=ns(input("\n¿Desea continuar?(n/s): "))
     if conti=="n":
         break
-    subprocess.call(["cmd.exe","/C","cls"])
     
