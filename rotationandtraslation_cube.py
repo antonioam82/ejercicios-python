07050004936d9afed7f594744279398c9f0489d5
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-05-23 19:31:53 +0200

    Update rotationandtraslation_cube.py

--- rotationandtraslation_cube.py
+++ rotationandtraslation_cube.py
@@ -1,171 +1,122 @@
 import glfw
 from OpenGL.GL import *
-from OpenGL.GL.shaders import compileProgram, compileShader
-import numpy as np
+import OpenGL.GL.shaders
+import numpy
 import pyrr
-from PIL import Image
 
 
-vertex_src = """
-# version 330
-layout(location = 0) in vec3 a_position;
-layout(location = 1) in vec2 a_texture;
-uniform mat4 model; // combined translation and rotation
-uniform mat4 projection;
-out vec3 v_color;
-out vec2 v_texture;
-void main()
-{
-    gl_Position = projection * model * vec4(a_position, 1.0);
-    v_texture = a_texture;
-}
-"""
+def main():
 
-fragment_src = """
-# version 330
-in vec2 v_texture;
-out vec4 out_color;
-uniform sampler2D s_texture;
-void main()
-{
-    out_color = texture(s_texture, v_texture);
-}
-"""
+    # initialize glfw
+    if not glfw.init():
+        return
+
+    window = glfw.create_window(800, 600, "My OpenGL window", None, None)
+
+    if not window:
+        glfw.terminate()
+        return
+
+    glfw.make_context_current(window)
+    #        positions        colors
+    cube = [-0.5, -0.5,  0.5, 1.0, 0.0, 0.0,#0.0
+             0.5, -0.5,  0.5, 0.0, 1.0, 0.0,
+             0.5,  0.5,  0.5, 0.0, 0.0, 1.0,
+            -0.5,  0.5,  0.5, 1.0, 1.0, 1.0,
+
+            -0.5, -0.5, -0.5, 1.0, 0.0, 0.0,
+             0.5, -0.5, -0.5, 0.0, 1.0, 0.0,
+             0.5,  0.5, -0.5, 0.0, 0.0, 1.0,
+            -0.5,  0.5, -0.5, 1.0, 1.0, 1.0]
+
+    cube = numpy.array(cube, dtype = numpy.float32)
+
+    indices = [0, 1, 2, 2, 3, 0,
+               4, 5, 6, 6, 7, 4,
+               4, 5, 1, 1, 0, 4,
+               6, 7, 3, 3, 2, 6,
+               5, 6, 2, 2, 1, 5,
+               7, 4, 0, 0, 3, 7]
+
+    indices = numpy.array(indices, dtype= numpy.uint32)
+
+    vertex_shader = """
+    #version 330
+    in vec3 position;
+    in vec3 color;
+    uniform mat4 transform;
+    out vec3 newColor;
+    void main()
+    {
+        gl_Position = transform * vec4(position, 1.0f);
+        newColor = color;
+    }
+    """
+
+    fragment_shader = """
+    #version 330
+    in vec3 newColor;
+    out vec4 outColor;
+    void main()
+    {
+        outColor = vec4(newColor, 1.0f);
+    }
+    """
+
+    #RESIZING
+    def window_resize(window, width, height):
+        glViewport(0, 0, width, height)
+
+    glfw.set_window_pos(window, 400, 200)#400
+
+    # set the callback function for window resize
+    glfw.set_window_size_callback(window, window_resize)
+        
+    shader = OpenGL.GL.shaders.compileProgram(OpenGL.GL.shaders.compileShader(vertex_shader, GL_VERTEX_SHADER),
+                                          OpenGL.GL.shaders.compileShader(fragment_shader, GL_FRAGMENT_SHADER))
+
+    #DRAW IN WIREFRAMES
+    #glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, 192, cube, GL_STATIC_DRAW)
+
+    EBO = glGenBuffers(1)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 144, indices, GL_STATIC_DRAW)
+
+    position = glGetAttribLocation(shader, "position")
+    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+    glEnableVertexAttribArray(position)
+
+    color = glGetAttribLocation(shader, "color")
+    glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+    glEnableVertexAttribArray(color)
 
 
-# glfw callback functions
-def window_resize(window, width, height):
-    glViewport(0, 0, width, height)
-    projection = pyrr.matrix44.create_perspective_projection_matrix(45, width / height, 0.1, 100)
-    glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
+    glUseProgram(shader)
 
-# initializing glfw library
-if not glfw.init():
-    raise Exception("glfw can not be initialized!")
+    glClearColor(0.2, 0.3, 0.2, 1.0)#0.2
+    glEnable(GL_DEPTH_TEST)
 
-# creating the window
-window = glfw.create_window(1280, 720, "My OpenGL window", None, None)
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
 
-# check if window was created
-if not window:
+        glClearColor(0, 0, 0, 0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        rot_x = pyrr.Matrix44.from_x_rotation(0.7 * glfw.get_time() )#0.5
+        rot_y = pyrr.Matrix44.from_y_rotation(1.0 * glfw.get_time() )#0.8
+
+        transformLoc = glGetUniformLocation(shader, "transform")
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, rot_x * rot_y)
+
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, None)
+
+        glfw.swap_buffers(window)
+
     glfw.terminate()
-    raise Exception("glfw window can not be created!")
 
-# set window's position
-glfw.set_window_pos(window, 400, 200)
-
-# set the callback function for window resize
-glfw.set_window_size_callback(window, window_resize)
-
-# make the context current
-glfw.make_context_current(window)
-
-vertices = [-0.5, -0.5,  0.5, 0.0, 0.0,
-             0.5, -0.5,  0.5, 1.0, 0.0,
-             0.5,  0.5,  0.5, 1.0, 1.0,
-            -0.5,  0.5,  0.5, 0.0, 1.0,
-
-            -0.5, -0.5, -0.5, 0.0, 0.0,
-             0.5, -0.5, -0.5, 1.0, 0.0,
-             0.5,  0.5, -0.5, 1.0, 1.0,
-            -0.5,  0.5, -0.5, 0.0, 1.0,
-
-             0.5, -0.5, -0.5, 0.0, 0.0,
-             0.5,  0.5, -0.5, 1.0, 0.0,
-             0.5,  0.5,  0.5, 1.0, 1.0,
-             0.5, -0.5,  0.5, 0.0, 1.0,
-
-            -0.5,  0.5, -0.5, 0.0, 0.0,
-            -0.5, -0.5, -0.5, 1.0, 0.0,
-            -0.5, -0.5,  0.5, 1.0, 1.0,
-            -0.5,  0.5,  0.5, 0.0, 1.0,
-
-            -0.5, -0.5, -0.5, 0.0, 0.0,
-             0.5, -0.5, -0.5, 1.0, 0.0,
-             0.5, -0.5,  0.5, 1.0, 1.0,
-            -0.5, -0.5,  0.5, 0.0, 1.0,
-
-             0.5, 0.5, -0.5, 0.0, 0.0,
-            -0.5, 0.5, -0.5, 1.0, 0.0,
-            -0.5, 0.5,  0.5, 1.0, 1.0,
-             0.5, 0.5,  0.5, 0.0, 1.0]
-
-indices = [ 0,  1,  2,  2,  3,  0,
-            4,  5,  6,  6,  7,  4,
-            8,  9, 10, 10, 11,  8,
-           12, 13, 14, 14, 15, 12,
-           16, 17, 18, 18, 19, 16,
-           20, 21, 22, 22, 23, 20]
-
-vertices = np.array(vertices, dtype=np.float32)
-indices = np.array(indices, dtype=np.uint32)
-
-shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), compileShader(fragment_src, GL_FRAGMENT_SHADER))
-
-# Vertex Buffer Object
-VBO = glGenBuffers(1)
-glBindBuffer(GL_ARRAY_BUFFER, VBO)
-glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
-
-# Element Buffer Object
-EBO = glGenBuffers(1)
-glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
-glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, GL_STATIC_DRAW)
-
-glEnableVertexAttribArray(0)
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, vertices.itemsize * 5, ctypes.c_void_p(0))
-
-glEnableVertexAttribArray(1)
-glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, vertices.itemsize * 5, ctypes.c_void_p(12))
-
-texture = glGenTextures(1)
-glBindTexture(GL_TEXTURE_2D, texture)
-
-# Set the texture wrapping parameters
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-# Set texture filtering parameters
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-
-# load image
-image = Image.open("dob.jpg")#"textures/crate.jpg")
-image = image.transpose(Image.FLIP_TOP_BOTTOM)
-img_data = image.convert("RGBA").tobytes()
-# img_data = np.array(image.getdata(), np.uint8) # second way of getting the raw image data
-glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
-
-glUseProgram(shader)
-glClearColor(0, 0.1, 0.1, 1)
-glEnable(GL_DEPTH_TEST)
-glEnable(GL_BLEND)
-glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-
-projection = pyrr.matrix44.create_perspective_projection_matrix(45, 1280/720, 0.1, 100)
-translation = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, 0, -3]))
-
-model_loc = glGetUniformLocation(shader, "model")
-proj_loc = glGetUniformLocation(shader, "projection")
-
-glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
-
-# the main application loop
-while not glfw.window_should_close(window):
-    glfw.poll_events()
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-    rot_x = pyrr.Matrix44.from_x_rotation(0.5 * glfw.get_time())
-    rot_y = pyrr.Matrix44.from_y_rotation(0.8 * glfw.get_time())
-
-    rotation = pyrr.matrix44.multiply(rot_x, rot_y)
-    model = pyrr.matrix44.multiply(rotation, translation)
-
-    glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
-
-    glDrawElements(GL_TRIANGLES, len(indices), GL_UNSIGNED_INT, None)
-
-    glfw.swap_buffers(window)
-
-# terminate glfw, free up allocated resources
-glfw.terminate()
+if __name__ == "__main__":
+    main()
