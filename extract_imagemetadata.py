5f8c2b7b948b090bf3edbdfafc4c46332ec13244
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-07-26 14:45:35 +0200

    Update extract_imagemetadata.py

--- extract_imagemetadata.py
+++ extract_imagemetadata.py
@@ -1,17 +1,18 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
 
-imagename = "image.jpg"
+imagename = "dob.jpg"
 
 image = Image.open(imagename)
 exifdata = image._getexif()
+#print(len(exifdata))
 
-for tag_id in exifdata:
-	# get the tag name, instead of human unreadable tag id
-    tag = TAGS.get(tag_id, tag_id)
-    data = exifdata.get(tag_id)
-    # decode bytes 
-    if isinstance(data, bytes):
-        data = data.decode()
-    print('{} {}'.format(tag,data))
-    #print(f'{tag:25}: {data}')
+if len(exifdata)>0:
+    for tag_id in exifdata:
+        # get the tag name, instead of human unreadable tag id
+	tag = TAGS.get(tag_id, tag_id)
+	data = exifdata.get(tag_id)
+        # decode bytes 
+	if isinstance(data, bytes):
+	    data = data.decode()
+	print('{} {}'.format(tag,data))
