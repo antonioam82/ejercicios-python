e79838033fdc39f0dd48226619ed0044d663051c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-07-27 15:15:24 +0200

    Update extract_imagemetadata.py

--- extract_imagemetadata.py
+++ extract_imagemetadata.py
@@ -1,20 +1,16 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
-import os
-os.chdir(r'/home/alfmar/Documentos/Antonio')
 
 while True:
-	imagename = raw_input("Image: ")
-	image = Image.open(imagename)
-	exifdata = image._getexif()
-	if exifdata is not None:
-	        for tag_id in exifdata:
-		    # get the tag name, instead of human unreadable tag id
-		    tag = TAGS.get(tag_id, tag_id)
-		    data = exifdata.get(tag_id)
-		    # decode bytes 
-		    if isinstance(data, bytes):
-			    data = data.decode()
-		    print('{} {}'.format(tag,data))
+    imagename = input("Image: ")
+    image = Image.open(imagename)
+    exifdata = image._getexif()
+    if exifdata is not None:
+	for tag_id in exifdata:
+		tag = TAGS.get(tag_id, tag_id)
+		data = exifdata.get(tag_id)
+                if isinstance(data, bytes):
+		    data = data.decode()
+                print('{} {}'.format(tag,data))
     else:
-		print("NO DATA")
+	print("NO DATA")
