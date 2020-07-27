6b2d318d8ddc8100a52cae77582098bb49c127e4
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-07-27 14:23:41 +0200

    Update extract_imagemetadata.py

--- extract_imagemetadata.py
+++ extract_imagemetadata.py
@@ -10,13 +10,13 @@ exifdata = image._getexif()
 #print(len(exifdata))
 
 if exifdata is not None:
-	for tag_id in exifdata:
-		# get the tag name, instead of human unreadable tag id
-		tag = TAGS.get(tag_id, tag_id)
-		data = exifdata.get(tag_id)
-		# decode bytes 
-		if isinstance(data, bytes):
-			data = data.decode()
-		print('{} {}'.format(tag,data))
+    for tag_id in exifdata:
+        # get the tag name, instead of human unreadable tag id
+	tag = TAGS.get(tag_id, tag_id)
+	data = exifdata.get(tag_id)
+	# decode bytes 
+	if isinstance(data, bytes):
+	    data = data.decode()
+	print('{} {}'.format(tag,data))
 else:
-	print("NO DATA")
+    print("NO DATA")
