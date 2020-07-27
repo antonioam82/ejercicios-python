69aba25f28063fdf431a3ba7710cf46c6cfaefc2
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-07-27 15:09:09 +0200

    Update extract_imagemetadata.py

--- extract_imagemetadata.py
+++ extract_imagemetadata.py
@@ -1,21 +1,20 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
+import os
+os.chdir(r'/home/alfmar/Documentos/Antonio')
 
-imagename = "dob.jpg"
-
-image = Image.open(imagename)
-exifdata = image._getexif()
-#print(len(exifdata))
-
-While True:
+while True:
+	imagename = raw_input("Image: ")
+	image = Image.open(imagename)
+	exifdata = image._getexif()
 	if exifdata is not None:
-		for tag_id in exifdata:
-            # get the tag name, instead of human unreadable tag id
-	        tag = TAGS.get(tag_id, tag_id)
-	        data = exifdata.get(tag_id)
-	        # decode bytes 
-	        if isinstance(data, bytes):
-				data = data.decode()
-	        print('{} {}'.format(tag,data))
+	        for tag_id in exifdata:
+		    # get the tag name, instead of human unreadable tag id
+		    tag = TAGS.get(tag_id, tag_id)
+		    data = exifdata.get(tag_id)
+		    # decode bytes 
+		    if isinstance(data, bytes):
+			    data = data.decode()
+		    print('{} {}'.format(tag,data))
     else:
 		print("NO DATA")
