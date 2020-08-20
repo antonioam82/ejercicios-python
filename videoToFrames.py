1fd784e543c6fd593607f5a396d6fc9ec6bfd672
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-08-20 17:06:00 +0200

    Update videoToFrames.py

--- videoToFrames.py
+++ videoToFrames.py
@@ -1,11 +1,16 @@
 #CREATING FRAMES FROM A VIDEO
+
+#LIBRARIES YOU NEED.
 import cv2
 import os
 
-root = input("Enter root to the video: ")
+#ENTER PATH TO VIDEO.
+root = input("Enter path to video: ")
 
+#READ THE VIDEO.
 cam = cv2.VideoCapture(root)
 
+#CREATE 'data' FOLDER IF IT NOT EXISTS.
 try:
     if not os.path.exists('data'):
         os.makedirs('data')
@@ -13,21 +18,27 @@ try:
 except OSError:
     print('Error: Creating directory of data')
 
+#CREATE FRAME COUNTER.
 currentframe = 0
 
 while(True):
-
+    #READ FROM FRAMES
     ret,frame = cam.read()
 
     if ret:
+        #IF VIDEO IS STILL LETF, CONTINUE CREATING FRAMES.
         name = './data/frame'+str(currentframe)+'.jpg'
         print('Creating...'+name)
-
+        
+        #WRITE THE EXTRACTED FRAME.
         cv2.imwrite(name,frame)
-
+        
+        #INCREASE FRAME COUNTER.
         currentframe += 1
     else:
+        #FINISH THE LOOP.
         break
 
+#FINISH THE PROGRAM.
 cam.release()
 cv2.destroyAllWindows()
