2c5e806ea469c91df50e27fd11bbc1cec31d9729
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-09-06 17:46:57 +0200

    Update CPU_meter.py

--- CPU_meter.py
+++ CPU_meter.py
@@ -9,9 +9,9 @@ def cpu_met():
 
 ventana = Tk()
 ventana.geometry("480x140")
-ventana.title("CPU Usage")
+ventana.title("Total CPU Usage")
 ventana.configure(bg='midnight blue')
-cpu_label = Label(ventana,bg="black",fg="green",font="Arial 30 bold")
+cpu_label = Label(ventana,bg="black",fg="green",anchor=NE,font="Arial 30 bold",width=6)
 cpu_label.place(x=230,y=20)
 
 digi=Label(ventana,text="CPU Usage:",font="arial 24 bold",bg='midnight blue',fg="white")
@@ -20,3 +20,4 @@ digi.place(x=20,y=25)
 cpu_met()
 
 ventana.mainloop()
+
