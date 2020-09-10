471269cb0c60b5dcf25239ee34e925da3432b6c6
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-09-10 19:10:55 +0200

    Create QtDigital_Clock.py

---
+++ QtDigital_Clock.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QTimer, QTime, Qt
+
+class AppDemo(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(250,150)
+
+        layout = QVBoxLayout()
+        fnt = QFont('Open Sans', 120, QFont.Bold)
+
+        self.lbl = QLabel()
+        self.lbl.setAlignment(Qt.AlignCenter)
+        self.lbl.setFont(fnt)
+        layout.addWidget(self.lbl)
+
+        self.setLayout(layout)
+
+        timer = QTimer(self)
+        
+        timer.timeout.connect(self.displayTime)
+        timer.start(1000)
+
+        
+
+    def displayTime(self):
+        currentTime = QTime.currentTime()
+
+        displayText = currentTime.toString('hh:mm:ss')
+        print(displayText)
+
+        self.lbl.setText(displayText)
+        
+
+
+app = QApplication(sys.argv)
+
+demo = AppDemo()
+demo.show()
+
+app.exit(app.exec_())
