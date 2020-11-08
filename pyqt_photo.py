13c2e0bd1dc92d612e6d6b92b1a7776eb12ca9d6
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-11-08 18:09:05 +0100

    Create pyqt_photo.py

---
+++ pyqt_photo.py
@@ -0,0 +1,34 @@
+from PyQt5.QtWidgets import (QWidget, QHBoxLayout, 
+    QLabel, QApplication)
+from PyQt5.QtGui import QPixmap
+import sys
+
+class Example(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):      
+
+        hbox = QHBoxLayout(self)
+        pixmap = QPixmap("photo.png")
+
+        lbl = QLabel(self)
+        lbl.setPixmap(pixmap)
+
+        hbox.addWidget(lbl)
+        self.setLayout(hbox)
+        
+        self.move(300, 200)
+        self.setWindowTitle('Red Rock')
+        self.show()        
+        
+        
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
