3bb9c1036c17814d64b5d438ba04b501e576fa5c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-01-19 16:04:51 +0100

    Update probaility_of_detection.py

--- probaility_of_detection.py
+++ probaility_of_detection.py
@@ -2,3 +2,11 @@ import random
 from collections import Counter
 import numpy as np
 import matplotlib.pyplot as plt
+
+NUM_EQUIV_VOLUMES = 1000
+MAX_CIVS = 5000
+TRIALS = 1000
+CIV_STEP_SIZE = 100
+
+x = []
+y = []
