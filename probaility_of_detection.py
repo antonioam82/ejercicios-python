c11653aa922efb3052d5a898cb62d787ed5658db
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-01-20 12:49:34 +0100

    Update probaility_of_detection.py

--- probaility_of_detection.py
+++ probaility_of_detection.py
@@ -12,6 +12,15 @@ x = []
 y = []
 
 for num_civs in range(2,MAX_CIVS + 2,CIV_STEP_SIZE):
-    print(num_civs)
+    #print(num_civs)
     civs_per_vol = num_civs/NUM_EQUIV_VOLUMES
-    print(civs_per_vol)
+    #print(civs_per_vol)
+    num_single_civs = 0
+
+    for trial in range(TRIALS):
+        locations = []
+        while len(locations)<num_civs:
+            location = random.randint(1,NUM_EQUIV_VOLUMES)
+            locations.append(location)
+    print(len(locations))
+
