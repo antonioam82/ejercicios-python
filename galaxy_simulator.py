e4e610cd810867ab2f4bce9729b0c3193350205f
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-01-22 15:54:20 +0100

    Update galaxy_simulator.py

--- galaxy_simulator.py
+++ galaxy_simulator.py
@@ -11,5 +11,9 @@ c = tk.Canvas(root,width=1000,height=800,bg="black")
 c.grid()
 c.configure(scrollregion=(-500,-400,500,400))
 
+DISC_RADIUS = 50000
+DISC_HEIGHT = 1000
+DISC_VOL = math.pi*DISC_RADIUS**2*DISC_HEIGHT
+
 root.mainloop()
 
