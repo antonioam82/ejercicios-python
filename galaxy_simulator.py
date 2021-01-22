b91616ae05097118aa84e72425ba797f57a2c1df
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-01-22 15:46:35 +0100

    Create galaxy_simulator.py

---
+++ galaxy_simulator.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from random import randint,uniform,random
+import math
+
+SCALE = 255
+NUM_CIVS = 15600000
+
+root = tk.Tk()
+root.title("Milky Way Galaxy")
+c = tk.Canvas(root,width=1000,height=800,bg="black")
+c.grid()
+
+root.mainloop()
