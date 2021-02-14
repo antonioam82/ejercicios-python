b7f9c4d9f56fbd69175bc5943c161a7571d13e59
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-02-14 14:31:16 +0100

    Create manipulate_cube_rotation.py

---
+++ manipulate_cube_rotation.py
@@ -0,0 +1,90 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+verticies = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+    )
+
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7)
+    )
+
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(verticies[vertex])
+
+    glEnd()
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+    glTranslatef(0.0,0.0,-5)
+
+    glRotatef(0, 0, 0, 0)
+
+    x=1
+    y=1
+    z=1
+    #c=0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x=0
+                    y=1
+                    z=0
+                if event.key == pygame.K_RIGHT:
+                    x=1
+                    y=0
+                    z=0
+                if event.key == pygame.K_UP:
+                    x=0
+                    y=0
+                    z=1
+                if event.key == pygame.K_DOWN:
+                    x=1
+                    y=1
+                    z=1
+                    
+        glRotatef(1, x, y, z)#(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        #b+=0.001
+        #c+=1
+        #print(c)
+        #if c == 600:
+            #n=n*(-1)
+            #c=0
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+main()
