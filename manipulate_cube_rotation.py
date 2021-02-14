21534df47819335cf823cfc8d3f14b4f71b371e1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-02-14 17:47:24 +0100

    Update manipulate_cube_rotation.py

--- manipulate_cube_rotation.py
+++ manipulate_cube_rotation.py
@@ -48,11 +48,11 @@ def main():
 
     glRotatef(0, 0, 0, 0)
 
-    x=1
-    y=1
-    z=1
-    m=2
-
+    x=0
+    y=0
+    z=0
+    m=0
+    #c=0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -82,7 +82,12 @@ def main():
                     
         glRotatef(m, x, y, z)#(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-
+        #b+=0.001
+        #c+=1
+        #print(c)
+        #if c == 600:
+            #n=n*(-1)
+            #c=0
         Cube()
         pygame.display.flip()
         pygame.time.wait(10)
