49891a374c78e105e7402cbaf9f0794ac3de859c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-03-07 22:06:22 +0100

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -73,3 +73,32 @@ def chooseRandomMoveFromList(board, movesList):
     else:
         return None
 
+def getComputerMove(board, computerLetter):
+    if computerLetter == 'X':
+        playerLetter = 'O'
+    else:
+        playerLetter = 'X'
+
+    for i in range(1, 10):
+        copy = getBoardCopy(board)
+        if isSpaceFree(copy, i):
+            makeMove(copy, computerLetter, i)
+            if isWinner(copy, playerLetter):
+                return i
+
+    for i in range(1, 10):
+        copy = getBoardCopy(board)
+        if isSpaceFree(copy, i):
+            makeMove(copy, playerLetter, i)
+            if isWinner(copy, playerLetter):
+                return i
+
+    move = chooseRandomMoveFromList(board, [1,3,7,9])
+    if move != None:
+        return move
+
+    if isSpaceFree(board, 5):
+        return 5
+
+    return chooseRandomMoveFromList(board, [2,4,6,8])
+
