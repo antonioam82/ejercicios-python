b66d219961d325c88d89fe02bc60a35b184b3bcc
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-08-09 14:39:21 +0200

    Create moving_blocks.py

---
+++ moving_blocks.py
@@ -0,0 +1,9 @@
+import pygame, sys, time
+from pygame.locals import *
+
+pygame.init()
+
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode(WINDOWWITH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption("Animation")
