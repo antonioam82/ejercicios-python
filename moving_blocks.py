f501e1344c829c4eb0abe5c3e43e7297e9b6047a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-08-09 14:44:47 +0200

    Update moving_blocks.py

--- moving_blocks.py
+++ moving_blocks.py
@@ -6,7 +6,7 @@ pygame.init()
 WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode(WINDOWWITH, WINDOWHEIGHT), 0, 32)
-pygame.display.set_caption("Moving Blocks")
+pygame.display.set_caption("Animation")
 
 DOWNLEFT = 1
 DOWNRIGHT = 3
@@ -19,3 +19,8 @@ BLACK = (0,0,0)
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
+
+b1 = {'rect':pygame.Rect(300, 80, 50, 100), 'color':RED, 'dir':UPRIGHT}
+b2 = {'rect':pygame.Rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
+b3 = {'rect':pygame.Rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
+blocks = [b1, b2, b3]
