326a1851fbdf7c65bbf81ca8448814e83424a44d
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-10-14 17:46:59 +0200

    Update display_HTML.py

--- display_HTML.py
+++ display_HTML.py
@@ -11,14 +11,16 @@ class app():
         self.root.geometry("900x600")
         self.root.configure(bg="gray70")
         self.html_display = sct.ScrolledText(self.root,width=105,height=27,bg="black",fg="white")
-        self.html_display.place(x=20,y=30)
+        self.html_display.place(x=20,y=37)
 
         self.url = StringVar()
         self.currentDir = StringVar()
         self.currentDir.set(os.getcwd())
 
         Entry(self.root,textvariable=self.currentDir,width=149).place(x=0,y=0)
-        Entry(self.root,textvariable=self.url,width=70).place(x=20,y=490)
+        Entry(self.root,textvariable=self.url,width=50,font=('arial',14)).place(x=20,y=505)
+        Button(self.root,text="GET HTML",width=89,bg="azure4").place(x=20,y=533)
+        Button(self.root,text="COPY URL",width=9).place(x=580,y=505)
         
         
         self.root.mainloop()
