bac7d9ee2de9c64e5d5456aff0e022c4620f09c5
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-10-15 18:52:48 +0200

    Update display_HTML.py

--- display_HTML.py
+++ display_HTML.py
@@ -1,6 +1,8 @@
 from tkinter import *
+from tkinter import messagebox
 import tkinter.scrolledtext as sct
 from bs4 import BeautifulSoup
+import threading
 import os
 import requests
 
@@ -19,11 +21,25 @@ class app():
 
         Entry(self.root,textvariable=self.currentDir,width=149).place(x=0,y=0)
         Entry(self.root,textvariable=self.url,width=50,font=('arial',14)).place(x=20,y=505)
-        Button(self.root,text="GET HTML",width=89,bg="azure4").place(x=20,y=533)
+        Button(self.root,text="GET HTML",width=89,bg="azure4",command=self.init_task).place(x=20,y=533)
         Button(self.root,text="COPY URL",width=9).place(x=580,y=505)
         
         
         self.root.mainloop()
+
+    def init_task(self):
+        t = threading.Thread(target=self.get_html)
+        t.start()
+
+    def get_html(self):
+        try:
+            web = self.url.get()
+            result = requests.get(web)
+            content = result.text
+            soup = BeautifulSoup(content, 'lxml')
+            self.html_display.insert(END,soup.prettify())
+        except Exception as e:
+            messagebox.showwarning("UNEXPECTED ERROR",str(e))
 
 if __name__=="__main__":
     app()
