fc424cd485f9c12e9a342d6223ecfa8972f43286
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-03-07 09:45:59 +0100

    Create bollinger_bands_ex2.py

---
+++ bollinger_bands_ex2.py
@@ -0,0 +1,29 @@
+import ta
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+import warnings
+warnings.filterwarnings("ignore")
+
+df = yf.Ticker("BTC-USD").history(period="max").reset_index()[["Date","Close"]]
+print(df.head())
+
+bol = ta.volatility.BollingerBands(df["Close"], window=20)#14
+
+df['M-AVG'] = bol.bollinger_mavg()#banda media movil
+df['Low Band'] = bol.bollinger_lband()#banda inferior
+#df['hband'] = bol.bollinger_hband()#banda superior
+df['High Band'] = bol.bollinger_hband()#banda superior
+
+plt.grid()
+plt.title("BTC-USD")
+plt.plot(df["Date"],df["Close"])
+plt.plot(df["Date"],df["M-AVG"], color="red")
+plt.plot(df["Date"],df["Low Band"], color="orange")
+plt.plot(df["Date"],df["High Band"], color="orange")
+plt.fill_between(df["Date"],df["Low Band"],df["High Band"], alpha=0.2, color="orange")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.legend(loc="best")
+
+plt.show()
