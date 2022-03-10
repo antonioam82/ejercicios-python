c487d3c267e8704a0fe049a3646b1ddd71efbe9c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-03-10 16:49:38 +0100

    Create daily_returns.py

---
+++ daily_returns.py
@@ -0,0 +1,30 @@
+import ta
+import matplotlib.pyplot as plt
+import yfinance as yf
+from ta.utils import dropna
+import warnings
+warnings.filterwarnings("ignore")
+
+ticker = "GOGL"
+
+df = yf.Ticker(ticker).history(period="max").reset_index()[["Date","Close"]]
+
+df = dropna(df)
+print(df.head())
+
+#bol = ta.volatility.BollingerBands(df["Close"], window=20)#14
+
+df['dlr'] = ta.others.DailyLogReturnIndicator(df["Close"]).daily_log_return()
+
+print(df["dlr"].head())
+
+plt.grid()
+plt.title(ticker+ "DAILY RETURNS" )
+plt.plot(df["Date"],df["dlr"], color="green")
+
+#plt.fill_between(df["Date"],df["Low Band"],df["High Band"], alpha=0.2, color="orange")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.legend(loc="best")
+
+plt.show()
