7f65a6b35ef30ca65fe03f9b66c89c500e408921
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-04-27 10:46:20 +0200

    Update terminal_progress_bar.py

--- terminal_progress_bar.py
+++ terminal_progress_bar.py
@@ -2,16 +2,17 @@ import math
 from colorama import Back, init
 
 init()
-
+print("LOADING...")
 def progress_bar(progress, total):
     percent = 100 * (progress / float(total))
-    bar = (Back.GREEN+' '+Back.RESET) * int(percent) + '_' * (100 - int(percent))
+    bar = (Back.GREEN+' '+Back.RESET) * int(percent) + '-' * (100 - int(percent))
     print(f"\r|{bar}| {percent:.2f}%", end="\r")
 
 numbers = [x * 5 for x in range(2000,3000)]
 results = []
 
 progress_bar(0, len(numbers))
+
 for i, x in enumerate(numbers):
     results.append(math.factorial(x))
     progress_bar(i + 1, len(numbers))
