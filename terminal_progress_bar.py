5cf831c688abb95e6654bd22ca226154ce698264
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-05-03 12:04:22 +0200

    Update terminal_progress_bar.py

--- terminal_progress_bar.py
+++ terminal_progress_bar.py
@@ -1,18 +1,22 @@
 import math
 from colorama import Back, init
 
+#INICIAR 'colorama'
 init()
-print("LOADING...")
-def progress_bar(progress, total):
-    percent = 100 * (progress / float(total))
-    bar = (Back.GREEN+' '+Back.RESET) * int(percent) + '-' * (100 - int(percent))
-    print(f"\r|{bar}| {percent:.2f}%", end="\r")
 
+print("CALCULANDO FACTORIALES...")
+def progress_bar(progress, total):
+    percent = 100 * (progress / float(total))#CALCULA PORCENTAJE
+    bar = (Back.GREEN+' '+Back.RESET) * int(percent) + '-' * (100 - int(percent))#DEFINE BARRA
+    print(f"\r|{bar}| {percent:.2f}%", end="\r")#MUESTRA BARRA + PORCENTAJE
+
+#GENERAR LISTA DE VALORES
 numbers = [x * 5 for x in range(2000,3000)]
+
+#RESULTADOS
 results = []
 
-progress_bar(0, len(numbers))
-
+#REALIZAR CALCULOS.
 for i, x in enumerate(numbers):
     results.append(math.factorial(x))
-    progress_bar(i + 1, len(numbers))
+    progress_bar(i + 1, len(numbers))#LLAMADA A FUNCIÓN 'progress_bar()'
