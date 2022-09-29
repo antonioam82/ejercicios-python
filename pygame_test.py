184acebdd3f94579e8d893249f7ef7cf5a8801b0
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-09-29 18:57:00 +0200

    Update pygame_test.py

--- pygame_test.py
+++ pygame_test.py
@@ -4,13 +4,14 @@ import sys
 pygame.init()
 
 screen = pygame.display.set_mode((600,500))
-
+color = (255,0,0)
 done = False
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+    pygame.draw.rect(screen,color,pygame.Rect(100,100,100,100))
     pygame.display.update()
 
 pygame.quit()  # quits pygame
