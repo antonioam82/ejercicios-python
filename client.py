e873c26665bfd5031ab115ac800132949f66e4e1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-10-03 09:48:59 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,15 +1,14 @@
 import socket
 
-#CONECTA, ENVIA Y RECIBE INFORMACIÓN DEVUELTA DESE "server.py"
-
-HOST = "127.0.0.1" #
-PORT = 65123 #Puerto de envio
+HOST = "localhost"#"127.0.0.1" #IP DEL SERVIDOR
+PORT = 65123 #PUERTO DE ENVIO
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
 
-    s.sendall(b"Hola mundo")
+    s.connect((HOST, PORT))     # SOLICITUD DE CONEXIÓN CON SERVIDOR
 
-    data = s.recv(1024)
+    s.sendall(b"Hola mundo")    # ENVIAR DATOS AL SERVIDOR
+    data = s.recv(1024)         # ESPERA A RECEPCIÓN DE INFORMACIÓN DEL SERVIDOR
 
-print("Recibido", repr(data))
+print("Recibida", repr(data))
+
