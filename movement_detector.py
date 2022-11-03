daed1bac2c53da5cd4d9c45fbe3fa46e02b0bbb1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-11-03 19:46:21 +0100

    Update movement_detector.py

--- movement_detector.py
+++ movement_detector.py
@@ -3,7 +3,7 @@ import cv2
 import imutils
 import winsound
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
@@ -33,7 +33,7 @@ while True:
 
     if alarm_mode:
         frame_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame_bw = cv2.GaussianBlur(frame_bw. (5, 5), 0)
+        frame_bw = cv2.GaussianBlur(frame_bw, (5, 5), 0)
 
         difference = cv2.absdiff(frame_bw, start_frame)
         threshold  = cv2.threshold(difference, 25, 255, cv2.THRESH_BINARY)[1]
@@ -48,4 +48,20 @@ while True:
         cv2.imshow("Cam", threshold)
     else:
         cv2.imshow("Cam", frame)
+
+    if alarm_counter > 20:
+        if not alarm:
+            alarm = True
+            threading.Thread(target=beep_alarm).start()
+            
+    key_pressed = cv2.waitKey(30)
+    if key_pressed == ord("t"):
+        alarm_mode = not alarm_mode
+        alarm_counter = 0
+    if key_pressed == ord("q"):
+        alarm_mode = False
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
