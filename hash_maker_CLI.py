e0603e88c570c164ce2b9b79e902f55bd3d16000
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-11-05 17:10:59 +0100

    Update hash_maker_CLI.py

--- hash_maker_CLI.py
+++ hash_maker_CLI.py
@@ -1,18 +1,16 @@
 import hashlib
 from colorama import Fore, Back, init
 
-# GENERAR CADENAS HASH A PARTIRDE TEXTO USANDO DIFERENTES ALGORITMOS
-
 class HASH:
     def generateHash(h):
         digest=h.hexdigest()
         return digest
-x = 0
+    
 init()
 algoritmos = ["md5","sha1","sha224","sha256","sha384","sha512"]
 
 print(Back.BLUE+"-"*56+"HASH CLI"+"-"*56+Back.RESET)
-while x<1:
+while True:
     print(Fore.GREEN+"Elija número de opción deseada:")
     print("1-Usar algoritmo MD5")
     print("2-Usar algoritmo SHA1")
@@ -22,8 +20,6 @@ while x<1:
     print("6-Usar algoritmo SHA512")
     print("7-Finalizar programa")
     nAlgoritmo=int(input("Introducir opción: "))
-
-    
 
     algoritmo = ""
     if nAlgoritmo != 7:
@@ -39,7 +35,6 @@ while x<1:
         print(""+Fore.YELLOW)
         print(hash1)
         print(""+Fore.RESET)
-        x=0
     else:
-        x=1
+        break
 print("PROGRAM FINISHED")
