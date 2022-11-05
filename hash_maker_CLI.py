b96b3fc962d9f6634c93ca47b50e06d63bfe4da7
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-11-05 17:04:05 +0100

    Update hash_maker_CLI.py

--- hash_maker_CLI.py
+++ hash_maker_CLI.py
@@ -7,28 +7,30 @@ class HASH:
         return digest
 x = 0
 init()
+algoritmos = ["md5","sha1","sha224","sha256","sha384","sha512"]
+
 print(Back.BLUE+"-"*56+"HASH CLI"+"-"*56+Back.RESET)
 while x<1:
     print(Fore.GREEN+"Elija número de opción deseada:")
-    print("1-Usar algoritmo SHA256")
-    print("2-Usar algoritmo SHA512")
-    print("3-Finalizar programa")
+    print("1-Usar algoritmo MD5")
+    print("2-Usar algoritmo SHA1")
+    print("3-Usar algoritmo SHA224")
+    print("4-Usar algoritmo SHA256")
+    print("5-Usar algoritmo SHA384")
+    print("6-Usar algoritmo SHA512")
+    print("7-Finalizar programa")
     nAlgoritmo=int(input("Introducir opción: "))
 
     
 
     algoritmo = ""
-    if nAlgoritmo != 3:
+    if nAlgoritmo != 7:
 
         datos=input("Introducir información a hashear: ")
 
-        if nAlgoritmo == 1:
-            algoritmo="sha256"
-        else:
-            algoritmo="sha512"
-        #elif nAlgoritmo == 2:
-            
 
+        algoritmo = algoritmos[nAlgoritmo-1]
+            
         bdatos = bytes(datos, 'utf-8')
         h = hashlib.new(algoritmo,bdatos)
         hash1=HASH.generateHash(h)
