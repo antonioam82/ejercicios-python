ec8a5c765babe69322d51488b7e366966aebb3fa
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-11-27 17:24:26 +0100

    Create encryption.py

---
+++ encryption.py
@@ -0,0 +1,19 @@
+from cryptography.fernet import Fernet
+
+def genera_clave():
+	clave = Fernet.generate_key()
+	with open("clave.key","wb") as archivo_clave:
+		archivo_clave.write(clave)
+
+def cargar_clave():
+	return open("clave.key","rb").read()
+
+genera_clave()
+clave = cargar_clave()
+mensaje = "Mi mensaje".encode()
+f = Fernet(clave)
+encriptado = f.encrypt(mensaje)
+print(encriptado)
+
+dessen = f.decrypt(encriptado)
+print(dessen)
