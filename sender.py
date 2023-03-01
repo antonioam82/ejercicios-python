cbb85572bf4284cf329afcc7843f700624b6ee62
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-03-01 17:57:57 +0100

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,18 +1,21 @@
 import os
 import socket
 
+HOST = "localhost" #IP DEL SERVIDOR
+PORT = 9999        #PUERTO DE ENVIO
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost",9999))
+client.connect((HOST, PORT)) # SOLICITUD DE CONEXION CON SERVIDOR
 
-file = open("image.png", "rb")
-file_size = os.path.getsize("image.png")
+file = open("image.png", "rb")  # ABRIR ARCHIVO A TRANSFERIR
+file_size = os.path.getsize("image.png") # OBTENER TAMAÑO ARCHIVO
 
-client.send("received_image.png".encode())
-client.send(str(file_size).encode())
+client.send("received_image.png".encode()) # ENVIAR NOMBRE DE NUEVO ARCHIVO
+client.send(str(file_size).encode())       # ENVIAR TAMAÑO ARCHIVO
 
-data = file.read()
-client.sendall(data)
-client.send(b"<END>")
+data = file.read()    # LEER INFORMACIÓN DEL ARCHIVO
+client.sendall(data)  # ENVIAR INFORMACIÓN DE ARCHIVO AL SERVIDOR
+client.send(b"<END>") # MARCADOR DE FIN DE DATOS
 
-file.close()
-client.close()
+file.close()   # CERRAR ARCHIVO
+client.close() # CERRAR CLIENTE
