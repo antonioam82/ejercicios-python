6217f04d696021ae4aed302741f3d3467b83d34a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-03-01 18:02:59 +0100

    Update sender.py

--- sender.py
+++ sender.py
@@ -10,7 +10,7 @@ client.connect((HOST, PORT)) # SOLICITUD DE CONEXION CON SERVIDOR
 file = open("image.png", "rb")  # ABRIR ARCHIVO A TRANSFERIR
 file_size = os.path.getsize("image.png") # OBTENER TAMAÑO ARCHIVO
 
-client.send("received_image.png".encode()) # ENVIAR NOMBRE DE NUEVO ARCHIVO
+client.send("copied_image.png".encode()) # ENVIAR NOMBRE DE NUEVO ARCHIVO
 client.send(str(file_size).encode())       # ENVIAR TAMAÑO ARCHIVO
 
 data = file.read()    # LEER INFORMACIÓN DEL ARCHIVO
