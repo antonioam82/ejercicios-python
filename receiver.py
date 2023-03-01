973fd2f61f8525a8dd8b6b1133e594fe57c310b0
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-03-01 17:59:01 +0100

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -2,27 +2,31 @@ import socket
 import tqdm
 import time
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("localhost", 9999))
-server.listen()
+HOST = "localhost" #IP DEL SERVIDOR
+PORT = 9999        #PUERTO DE ENVIO
 
-client, addr = server.accept()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))     # ASOCIAR SOCKET A DIRECCIÓN IP Y PUERTO
+server.listen()               # PONER SERVIDOR A LA ESCUCHA 
+
+client, addr = server.accept() # ACEPTAR CONEXIONES ENTRANTES
 
 file_name = client.recv(1024).decode()
-print(file_name)
+print(file_name)                      # IMPRIME NOMBRE DEL NUEVO ARCHIVO
 file_size = client.recv(1024).decode()
-print(file_size)
+print(file_size)                      # IMPRIME TAMAÑO ARCHIVO
 
-file = open(file_name, "wb")
+file = open(file_name, "wb")  # ABRE ARCHIVO EN MODO ESCRITURA
 
 file_bytes = b""
 
-size = int(file_size)
+size = int(file_size) # TAMAÑO DEL ARCHIVO RECIVIDO
 
 done = False
 
+# CREAR BARRA DE PROGRESO
 progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000,
-                total=size)
+                total=size) 
 
 while not done:
     data = client.recv(1024)
@@ -31,10 +35,11 @@ while not done:
     else:
         file_bytes += data
     time.sleep(0.003)
-    progress.update(1024)
+    progress.update(1024) # ACTUALIZAR BARRA DE PROGRESO
 
-file.write(file_bytes)
+file.write(file_bytes) # ESCRIBIR ARCHIVO
 
-file.close()
-client.close()
-server.close()
+file.close()    # CERRAR ARCHIVO
+client.close()  # CERRAR CLIENTE
+server.close()  # CERRAR SERVIDOR
+
