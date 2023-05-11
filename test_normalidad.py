ba03af1f820b615e07603d31193ea89b644660a8
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-05-11 16:08:30 +0200

    Create test_normalidad.py

---
+++ test_normalidad.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import scipy
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis, chi2
+
+'''
+ Goal: Create a normality test e.g. Jarque-Bera
+
+ step 1: generate random variables
+ step 2: visualise histogram
+ step 3: what is Jarque-Bera
+
+ '''
+
+# SIMULATE RANDOM VARIABLES
+x_size = 10**6
+degrees_freedom = 9
+type_random_variable = "normal"
+
+if type_random_variable == "normal":
+    x = np.random.standard_normal(x_size)
+    x_str = type_random_variable
+elif type_random_variable == "exponential":
+    x = np.random.standard_exponential(x_size)
+    x_str = type_random_variable
+elif type_random_variable == "student":
+    x = np.random.standard_t(df=degrees_freedom,size=x_size)
+    x_str = type_random_variable + ' (df=' + str(degrees_freedom) + ')'
+elif type_random_variable == "chi-squared":
+    x = np.random.chisquare(size=x_size,df=2)
+    x_str = type_random_variable + ' (df=' + str(degrees_freedom) + ')'
+    
+print(" ")
+# COMPUTE RISK METRICS
+x_mean = np.mean(x)
+s_stdev = np.std(x)
+x_skew = skew(x)
+x_kurt = kurtosis(x)
+x_median = np.percentile(x,95)
+x_VaR95 = np.percentile(x,5)
+x_jb = x_size/6*(x_skew**2 + 1/4*x_kurt**2)
+p_value = 1 - chi2.cdf(x_jb,df=degrees_freedom)
+is_normal = (p_value > 0.05)
+
+print("MEAN: ",x_mean)
+print("STD_DEV: ",s_stdev)
+print("SKEWNESS: ",x_skew)
+print("KURTOSIS: ",x_kurt)
+print("MEDIAN: ",x_median)
+print("VaR95: ",x_VaR95)
+print("JARQUE-BERA: ",x_jb)
+print("P-VALUE: ",p_value)
+print("IS NORMAL: ",str(is_normal))
+
+# PLOT HISTOGRAM
+plt.figure()
+plt.hist(x,bins=100)
+plt.title('Histogram ' + x_str)
+plt.show()
