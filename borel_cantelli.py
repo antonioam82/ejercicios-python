9f999afdb19b9511e4f6b48cdcaa5c6960d2609e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-05-13 17:04:34 +0200

    Update borel_cantelli.py

--- borel_cantelli.py
+++ borel_cantelli.py
@@ -1,9 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib as mpl
 import scipy
-import matplotlib
-import matplotlib.pyplot as plt
 from scipy.stats import skew, kurtosis, chi2
 
 '''
@@ -18,7 +15,7 @@ counter = 0
 ### Borel-Cantellí: P[all test are normal = True] = 0
 while is_normal == True and counter < int(times):
     x_size = 10**6
-    degrees_freedom = 5
+    degrees_freedom = 1
     # GENERATE DISTRIBUTION
     x = np.random.standard_normal(x_size) 
     
@@ -30,6 +27,7 @@ while is_normal == True and counter < int(times):
     x_kurt = kurtosis(x)
     x_median = np.percentile(x,95)
     x_VaR95 = np.percentile(x,5)
+    x_cvar95 = np.mean(x[x <= x_VaR95])
     x_jb = x_size/6*(x_skew**2 + 1/4*x_kurt**2)
     p_value = 1 - chi2.cdf(x_jb,df=degrees_freedom)
     is_normal = (p_value > 0.05)
@@ -40,6 +38,7 @@ while is_normal == True and counter < int(times):
     print("KURTOSIS: ",x_kurt)
     print("MEDIAN: ",x_median)
     print("VaR95: ",x_VaR95)
+    print("CVaR95: ",x_cvar95)
     print("JARQUE-BERA: ",x_jb)
     print("P-VALUE: ",p_value)
     print("IS NORMAL: ",str(is_normal))
