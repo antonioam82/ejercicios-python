2b394bce3bc38550958f0c4c5898c9b9289f400f
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-09-02 11:05:44 +0200

    Update SVD2.py

--- SVD2.py
+++ SVD2.py
@@ -3,13 +3,29 @@ import numpy as np
 from scipy.linalg import svd
 import argparse
 from colorama import init, Fore, Style
+import os
+
+init()
+
+image_formats = ['.png','.jpg']
+
+def check_file(file):
+    if file in os.listdir():
+        name, ex = os.path.splitext(file)
+        
+        if ex in image_formats:
+            return file
+        else:
+            raise argparse.ArgumentTypeError(Fore.RED + Style.BRIGHT + f"source file must be a supported image format ('.png' or '.jpg')." + Fore.RESET + Style.RESET_ALL)
+    else:
+        raise argparse.ArgumentTypeError(Fore.RED + Style.BRIGHT + f"file '{file}' not found." + Fore.RESET + Style.RESET_ALL)
 
 
 def main():
     parser = argparse.ArgumentParser(prog="SVD2",description="Programa para reducir la dimensiomalidad de una imagen.")
-    parser.add_argument('-src', '--source', required=True, help='Imagen fuente')
+    parser.add_argument('-src', '--source', type=check_file, required=True, help='Imagen fuente')
     parser.add_argument('-dest', '--destination', default="output_image.mp4", help='Imagen reducida')
-    parser.add_argument('-sigb', '--signif_bytes', required=True, help='Número de bytes significativos para apliación de reducción')
+    parser.add_argument('-sigb', '--signif_bytes', required=False, help='Número de bytes significativos para apliación de reducción')
 
     args = parser.parse_args()
 
