73a86c24916571361f9723bcb30d34d41789bb87
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-09-04 22:01:20 +0200

    Update SVD2.py

--- SVD2.py
+++ SVD2.py
@@ -27,9 +27,10 @@ def main():
     parser = argparse.ArgumentParser(prog="SVD2",description="Programa para reducir la dimensiomalidad de una imagen.")
     parser.add_argument('-src', '--source', type=check_file, required=True, help='Imagen fuente')
     parser.add_argument('-dest', '--destination', default="output_image.mp4", help='Imagen reducida')
-    parser.add_argument('-sigb', '--signif_bytes', required=True, help='Número de bytes significativos para apliación de reducción')
+    parser.add_argument('-sigb', '--signif_bytes',type=int, required=True, help='Número de bytes significativos para apliación de reducción')
 
     args = parser.parse_args()
+
 
 if __name__ == "__main__":
     main()
