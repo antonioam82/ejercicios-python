09ae95abf106df99b16284beb1e25017b9887e6a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-09-04 22:07:39 +0200

    Update SVD2.py

--- SVD2.py
+++ SVD2.py
@@ -23,10 +23,11 @@ def check_file(file):
         raise argparse.ArgumentTypeError(Fore.RED + Style.BRIGHT + f"file '{file}' not found." + Fore.RESET + Style.RESET_ALL)
 
 
+
 def main():
     parser = argparse.ArgumentParser(prog="SVD2",description="Programa para reducir la dimensiomalidad de una imagen.")
     parser.add_argument('-src', '--source', type=check_file, required=True, help='Imagen fuente')
-    parser.add_argument('-dest', '--destination', default="output_image.mp4", help='Imagen reducida')
+    parser.add_argument('-dest', '--destination', default="output_image.png", help='Imagen reducida')
     parser.add_argument('-sigb', '--signif_bytes',type=int, required=True, help='Número de bytes significativos para apliación de reducción')
 
     args = parser.parse_args()
