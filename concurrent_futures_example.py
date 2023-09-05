2cdd0d97bbd3064d49539fb2c6f3c661f1fcbe83
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-09-05 16:16:07 +0200

    Create concurrent_futures_example.py

---
+++ concurrent_futures_example.py
@@ -0,0 +1,52 @@
+'''import time
+import random
+
+def mail_letter(letter):
+    #duration = random.randint(1, 5)
+    duration = 2
+    print(f"Started mailing letter {letter} (duration: {duration}s)")
+    time.sleep(duration)
+    print(f"Finished mailing letter {letter}")
+    return f"Letter {letter} mailed"
+
+if __name__ == '__main__':
+    inicio = time.time()
+    letters = ['A', 'B', 'C', 'D', 'E']
+    results = []
+
+    for letter in letters:
+        result = mail_letter(letter)
+        results.append(result)
+
+    print("Mailing Results:")
+    for result in results:
+        print(result)
+    fin = time.time()
+    tiempo_total = fin - inicio
+    print("Duration: ",tiempo_total)'''
+########################################################3
+import concurrent.futures
+import time
+import random
+
+def mail_letter(letter):
+    #duration = random.randint(1, 5)
+    duration = 2
+    print(f"Started mailing letter {letter} (duration: {duration}s)")
+    time.sleep(duration)
+    print(f"Finished mailing letter {letter}")
+    return f"Letter {letter} mailed"
+
+if __name__ == '__main__':
+    inicio = time.time()
+    letters = ['A', 'B', 'C', 'D', 'E']
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(executor.map(mail_letter, letters))
+
+    print("Mailing Results:")
+    for result in results:
+        print(result)
+    fin = time.time()
+    tiempo_total = fin - inicio
+    print("Duration: ",tiempo_total)
