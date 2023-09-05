a35ad52a565b4a97e47b9b29bc243906291f6d15
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-09-05 15:35:10 +0200

    Update SVD2.py

--- SVD2.py
+++ SVD2.py
@@ -37,9 +37,9 @@ def redux(args):
     num_bytes = m*n*3
     print("Number of bytes: ",num_bytes)
     B,G,R = cv2.split(image)
-    svd_f(B,G,R)
+    svd_f(B,G,R,args.signif_bytes,m,n)
 
-def svd_f(B,G,R):
+def svd_f(B,G,R,k,m,n):
     U_B, s_B, V_transp_B = svd(B, full_matrices=False)
     U_G, s_G, V_transp_G = svd(G, full_matrices=False)
     U_R, s_R, V_transp_R = svd(R, full_matrices=False)
@@ -58,7 +58,31 @@ def svd_f(B,G,R):
     for i in range(len(s_R)):
         a = (s_R[i]/(sum(s_R)))*100
         sigma_s_R.append(a)
-        
+
+    U_B_k = U_B[:,:k]
+    Sigma_B_k = np.diag(s_B[:k])
+    V_transp_B_k = V_transp_B[:k,:]
+
+    U_G_k = U_G[:,:k]
+    Sigma_G_k = np.diag(s_G[:k])
+    V_transp_G_k = V_transp_G[:k,:]
+
+    U_R_k = U_R[:,:k]
+    Sigma_R_k = np.diag(s_R[:k])
+    V_transp_R_k = V_transp_R[:k,:]
+
+    B_reducida = U_B_k.dot(Sigma_B_k.dot(V_transp_B_k))
+    G_reducida = U_G_k.dot(Sigma_G_k.dot(V_transp_G_k))
+    R_reducida = U_R_k.dot(Sigma_R_k.dot(V_transp_R_k))
+
+    Imagen_SVD = np.zeros((m,n,3), dtype='uint8')
+ 
+    Imagen_SVD[:,:,0] = B_reducida
+    Imagen_SVD[:,:,1] = G_reducida
+    Imagen_SVD[:,:,2] = R_reducida
+
+    Imagen_con_SVD = cv2.merge([Imagen_SVD[:,:,0],Imagen_SVD[:,:,1],Imagen_SVD[:,:,2]])
+     
     print('done')
 
 def main():
