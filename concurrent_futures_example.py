e11af19adadf943a4782c2cef7bea2c66b80b644
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-09-05 16:21:30 +0200

    Update concurrent_futures_example.py

--- concurrent_futures_example.py
+++ concurrent_futures_example.py
@@ -30,8 +30,8 @@ import time
 import random
 
 def mail_letter(letter):
-    #duration = random.randint(1, 5)
-    duration = 2
+    duration = random.randint(1, 5)
+    #duration = 2
     print(f"Started mailing letter {letter} (duration: {duration}s)")
     time.sleep(duration)
     print(f"Finished mailing letter {letter}")
@@ -50,3 +50,26 @@ if __name__ == '__main__':
     fin = time.time()
     tiempo_total = fin - inicio
     print("Duration: ",tiempo_total)
+
+###########################################################
+'''import concurrent.futures
+import time
+import random
+
+def mail_letter(letter):
+    duration = random.randint(1, 5)
+    print(f"Started mailing letter {letter} (duration: {duration}s)")
+    time.sleep(duration)
+    print(f"Finished mailing letter {letter}")
+    return f"Letter {letter} mailed"
+
+if __name__ == '__main__':
+    letters = ['A', 'B', 'C', 'D', 'E']
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = {executor.submit(mail_letter, letter): letter for letter in letters}
+
+        for future in concurrent.futures.as_completed(futures):
+            letter = futures[future]
+            result = future.result()
+            print(f"Result: {result}")'''
