3893292df622a082b2717cb5a5ee8c2eff48a096
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-10-25 09:31:56 +0200

    Update to_do_app.py

--- to_do_app.py
+++ to_do_app.py
@@ -1,6 +1,6 @@
 import os
 import tkinter as tk
-from tkinter import ttk, Tk
+from tkinter import ttk, Tk, Listbox, Canvas
 
 class ToDo:
     def __init__(self):
@@ -12,9 +12,12 @@ class ToDo:
         current_dir.set(os.getcwd())
 
         tk.Entry(self.root, textvariable=current_dir,width=149).place(x=0,y=0)
-
+        self.canvas = Canvas(self.root,bg="blue",width=500,height=500)
+        self.canvas.place(x=9,y=27)
+        #self.todo_list = Listbox(
         
 
 
 if __name__ == "__main__":
     ToDo()
+
