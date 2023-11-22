7adc633f57822a30b27f5b115e8e20ab973cd3fd
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-11-22 20:16:13 +0100

    Create merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,41 @@
+
+# Función de ordenación
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        # Fusionar las dos mitades ordenadas
+        i = j = k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+    return arr
+
+
+# Elementos desordenados
+lista_elementos = [23, 6, 33, 1, 9, 4, 55, 82, 11, 3]
+
+# Elementos ordenados
+lista_ordenada = merge_sort(lista_elementos)
+print(lista_ordenada)
