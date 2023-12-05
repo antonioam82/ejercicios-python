9997ad642239d1cc09dd752ebe836b78028d8546
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-12-05 16:02:05 +0100

    Create random_tone_maker.py

---
+++ random_tone_maker.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import random
+from pydub import AudioSegment
+from pydub.generators import Sine, Square, Triangle, Sawtooth
+from pydub.playback import play
+import tkinter as tk
+from tkinter import Tk, filedialog, messagebox, ttk
+import os
+import threading
+
+class App:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Tone maker")
+        self.root.geometry("800x600")
+        self.root.resizable(height=False,width=False)
+
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    App()
