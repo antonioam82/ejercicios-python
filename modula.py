c86fbbc2f21d2bd029c987ea85920b1dba126bb0
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-12-21 16:04:55 +0100

    Update modula.py

--- modula.py
+++ modula.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.io import wavfile
+from scipy.signal import sawtooth
 import argparse
 from playsound import playsound
 from colorama import init, Fore, Style
@@ -8,9 +9,12 @@ import os
 
 init()
 
-def write_data(name, duration, sample_rate, frequency, modulation_rate):
+signals = ["sin","sqrt","trg","swt"]
+
+def write_data(name, signal, duration, sample_rate, frequency, modulation_rate):
     with open(name.replace('.wav', '_data.txt'), 'w') as file:
         file.write(f"Name: {name}\n")
+        file.write(f"Signal: {signal}\n")
         file.write(f"Duration: {duration} seconds\n")
         file.write(f"Sample Rate: {sample_rate} Hz\n")
         file.write(f"Frequency: {frequency} Hz\n")
@@ -23,17 +27,31 @@ def check_extension(file):
     else:
         raise argparse.ArgumentTypeError(Fore.RED + Style.BRIGHT + f"result file must have '.wav' extension." + Fore.RESET + Style.RESET_ALL)
 
+def check_type(t):
+    if t in signals:
+        return t
+    else:
+        raise argparse.ArgumentTypeError(Fore.RED + Style.BRIGHT + "Ivalid signal type: Must be 'sin', 'sqrt', 'trg' or 'swt'." + Fore.RESET + Style.RESET_ALL)
+    
 def generate_tone(args):
     name = args.destination
     duration = args.duration
     sample_rate = args.sample_rate
     frequency = args.frequency
     modulation_rate = args.modulation_rate
-    
+    signal = args.signal
     t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
-    carrier_wave = np.sin(2 * np.pi * frequency * t)
 
-    modulation_wave = np.sin(2 * np.pi * modulation_rate * t)
+    if signal == "sin":
+        modulation_wave = np.sin(2 * np.pi * frequency * t)
+    elif signal == "sqrt":
+        modulation_wave = np.sign(np.sin(2 * np.pi * 10 * t))
+    elif signal == "trg":
+        modulation_wave  = np.abs(sawtooth(2 * np.pi * 8 * t, width=0.5)) - 0.5  # 8 Hz
+    elif signal == "swt":
+        modulation_wave = sawtooth(2 * np.pi * 6 * t)
+
+    #modulation_wave = np.sin(2 * np.pi * modulation_rate * t)
     modulated_wave = np.sin(2 * np.pi * (frequency + modulation_rate * modulation_wave) * t)
     modulated_wave /= np.max(np.abs(modulated_wave), axis=0)
     wavfile.write(name, sample_rate, np.int16(modulated_wave * 32767))
@@ -44,7 +62,7 @@ def generate_tone(args):
         playsound(name)
 
     if args.write_data:
-        write_data(name, duration, sample_rate, frequency, modulation_rate)
+        write_data(name, signal, duration, sample_rate, frequency, modulation_rate)
         print("Created data file, correctly")
 
 def main():
@@ -56,7 +74,8 @@ def main():
     parser.add_argument('-mr','--modulation_rate',type=int,default=12,help="Modulation rate in Hz")
     parser.add_argument('-play','--play_audio',action='store_true',help="Play modulated signal")
     parser.add_argument('-wr','--write_data',action='store_true',help="Create text file with audio data")
-
+    parser.add_argument('-sig','--signal',default='sin',type=check_type,help="Modulation wave")
+    
     args = parser.parse_args()
     try:
         print(pyfiglet.figlet_format("modula",font="larry3d"))
@@ -66,4 +85,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
