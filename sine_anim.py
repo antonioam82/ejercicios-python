8863971036af1161f72119c24eebdf90aa82cbce
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-03-09 10:34:17 +0100

    Update sine_anim.py

--- sine_anim.py
+++ sine_anim.py
@@ -24,12 +24,13 @@ if __name__ == "__main__":
     main()'''
 
 ####################################################
-
 import plotext
 import numpy as np
 
 def main():
-    l = 10
+    amplitude = 1
+    frequency = 0.2
+    length = 1000
     frames = 50
 
     plotext.title("Sine Animation")
@@ -39,18 +40,15 @@ def main():
         plotext.clt()
         plotext.cld()
 
-        x = np.linspace(0, 10, l)
+        x = np.linspace(0, 10, length)
+        phase = 2 * np.pi * i / frames  # Variamos la fase en cada frame
 
-        # Ajustar la frecuencia, fase y amplitud
-        frequency = 1#0.3  # Frecuencia original
-        phase = 0  # Fase original
-        amplitude = 1  # Amplitud original
-
-        y = amplitude * np.sin(frequency * x + 2 * np.pi * i / frames + phase)
+        y = amplitude * np.sin(frequency * x + phase)
         
-        plotext.xlim(0, 10)
         plotext.plot(x, y, marker="dot", color="red")
-        plotext.sleep(0.05)
+        plotext.ylim(-1.0, 1.0)  # Limitamos el eje y a un rango fijo
+        plotext.xlim(0, 10)      # Limitamos el eje x a un rango fijo
+        plotext.sleep(0.1)
         plotext.show()
 
 if __name__ == "__main__":
