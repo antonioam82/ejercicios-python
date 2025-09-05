e215e3b5851e8aaa74d5d2203b1565f35a569207
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2025-09-05 10:14:40 +0200

    Update retornos.py

--- retornos.py
+++ retornos.py
@@ -1,20 +1,21 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
+import numpy as np
 
 ticker = "AAPL"
 
 # Descarga con rango específico
-data = yf.download(ticker, start="2024-01-01", end="2024-08-01", auto_adjust=False)
+data = yf.download(ticker, start="2024-01-01", end="2024-12-31", auto_adjust=False)
 
-# Calcular retornos diarios
-data['Retorno'] = data['Adj Close'].pct_change()
+# Calcular retornos logarítmicos
+data['Retorno_Log'] = np.log(data['Adj Close'] / data['Adj Close'].shift(1))
 
 # Graficar
 plt.figure(figsize=(12,6))
-plt.plot(data.index, data['Retorno'], label=f"Retornos diarios de {ticker}")
+plt.plot(data.index, data['Retorno_Log'], label=f"Retornos logarítmicos de {ticker}")
 plt.axhline(0, color='red', linestyle='--', linewidth=1)
-plt.title(f"Retornos diarios de {ticker}")
+plt.title(f"Retornos logarítmicos de {ticker}")
 plt.xlabel("Fecha")
-plt.ylabel("Retorno diario (%)")
+plt.ylabel("Retorno logarítmico")
 plt.legend()
 plt.show()
