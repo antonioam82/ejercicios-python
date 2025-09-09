e2594015d7c803b05b8a4cc95419fdbace6fa85e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2025-09-09 10:19:14 +0200

    Update retornos.py

--- retornos.py
+++ retornos.py
@@ -4,16 +4,21 @@ import numpy as np
 
 ticker = "AAPL"
 
-# Descarga con rango específico
-data = yf.download(ticker, start="2024-01-01", end="2024-12-31", auto_adjust=False)
+# Descarga de datos
+data = yf.download(ticker, start="2024-01-01",
+                   end="2024-12-31", auto_adjust=False)
+
+#print(data) # muestra datos
 
 # Calcular retornos logarítmicos
 data['Retorno_Log'] = np.log(data['Adj Close'] / data['Adj Close'].shift(1))
+print(data['Retorno_Log'])
 
 # Graficar
 plt.figure(figsize=(12,6))
 plt.plot(data.index, data['Retorno_Log'], label=f"Retornos logarítmicos de {ticker}")
 plt.axhline(0, color='red', linestyle='--', linewidth=1)
+plt.grid()
 plt.title(f"Retornos logarítmicos de {ticker}")
 plt.xlabel("Fecha")
 plt.ylabel("Retorno logarítmico")
