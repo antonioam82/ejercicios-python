77d51bfd0f7efca8d683e7dce0ac1a5ba4856a78
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2026-09-04 10:43:49 +0200

    Add write_data function and update argument parser

--- modula_tel.py
+++ modula_tel.py
@@ -9,6 +9,20 @@ from colorama import init, Fore, Style
 import sounddevice as sd
 import os
 
+def write_data(name, signal, duration, sample_rate, frequency, modulation_rate, scale, lfo):
+    base_name, ex = os.path.splitext(name)
+    
+    with open(name.replace('.wav', '_data.txt'), 'w') as file:
+        file.write(f"Name: {name}\n")
+        file.write(f"Signal: {signal}\n")
+        file.write(f"Duration: {duration} seconds\n")
+        file.write(f"Sample Rate: {sample_rate} Hz\n")
+        file.write(f"Frequency: {frequency} Hz\n")
+        file.write(f"Modulation Rate: {modulation_rate} Hz\n")
+        file.write(f"Scale: {scale}\n")
+        file.write(f"LFO: {lfo}")
+    print(f"Saved signal info in '{base_name}_data.txt'")
+
 def check_extension(file):
     name, ex = os.path.splitext(file)
     if ex == ".wav":
@@ -16,20 +30,58 @@ def check_extension(file):
     else:
         raise argparse.ArgumentTypeError(Fore.RED + Style.BRIGHT + f"result file must have '.wav' extension." + Fore.RESET + Style.RESET_ALL)
 
+def play(file_name):
+    sample_rate, audio_data = wavfile.read(file_name)
+    sd.play(audio_data, sample_rate)
+    sd.wait()
+    
+def generate_tone(args):
+    name = args.destination
+    duration = args.duration
+    sample_rate = args.sample_rate
+    frequency = args.frequency
+    modulation_rate = args.modulation_rate
+    signal = args.signal
+    scale = args.scale
+    lfo = args.lfo_rate
+    write = args.write_data
+
+    t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+ 
+    if signal == "sqrt":
+        modulation_wave = np.sign(np.sin(2 * np.pi * lfo * t)) #10
+    elif signal == "sin":
+        modulation_wave = np.sin(2 * np.pi * lfo * t)
+    elif signal == "trg":
+        modulation_wave  = np.abs(sawtooth(2 * np.pi * lfo * t, width=0.5)) - 0.5  # 8 Hz
+    elif signal == "swt":
+        modulation_wave = sawtooth(2 * np.pi * lfo * t) #6
+
+    
+    modulated_wave = np.sin(2 * np.pi * (frequency + modulation_rate * modulation_wave) * t)
+    modulated_wave /= np.max(np.abs(modulated_wave), axis=0)
+    wavfile.write(name, sample_rate, np.int16(modulated_wave * scale))
+    if write:
+        write_data(name, signal, duration, sample_rate, frequency, modulation_rate, scale, lfo)
+    
+
 def main():
-    parser = argparse.ArgumentParser(prog="MODULA 0.1", description="Generate modulated audio tones and phone ring tones")
+    parser = argparse.ArgumentParser(prog="MODULA_TEL 0.1", description="Generate modulated audio tones and phone ring tones")
     parser.add_argument('-dest', '--destination', type=check_extension, default="modulated_audio_signal.wav", help="Destination file name")
     parser.add_argument('-dur', '--duration', type=int, default=2, help="Audio duration, in seconds (ignored in ring mode)")
     parser.add_argument('-sr', '--sample_rate', type=int, default=44100, help="Sample rate in Hz")
+    parser.add_argument('-lfo', '--lfo_rate', type=int, default=10, help="Low Frequency Oscillator value")
     parser.add_argument('-freq', '--frequency', type=int, default=440, help="Base frequency in Hz")
     parser.add_argument('-mr', '--modulation_rate', type=int, default=12, help="Modulation rate in Hz (ignored in ring mode)")
     parser.add_argument('-play', '--play_audio', action='store_true', help="Play modulated signal")
     parser.add_argument('-wr', '--write_data', action='store_true', help="Create text file with audio data")
-    parser.add_argument('-sig', '--signal', default='sin', choices=['sin', 'sqrt', 'trg', 'swt'], help="Modulation wave (ignored in ring mode)")
+    parser.add_argument('-sig', '--signal', default='sqrt', choices=['sin', 'sqrt', 'trg', 'swt'], help="Modulation wave (ignored in ring mode)")
     parser.add_argument('-scl', '--scale', default=32767, type=int, help="Sound scale")
 
     args = parser.parse_args()
-
+    generate_tone(args)
+    if args.play_audio:
+        play(args.destination)
 
 if __name__ == '__main__':
     main()
